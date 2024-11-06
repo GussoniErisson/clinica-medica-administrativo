8a97af6faacf63f276cb8a6f23bb6d0df7c35d36
GussoniErisson <erisson.gussoni@aluno.imepac.edu.br>
2024-11-06 20:41:52 -0300

    Entidade Perfil

---
+++ src/main/java/br/edu/imepac/administrativo/entidades/Perfil.java
@@ -0,0 +1,33 @@
+package br.edu.imepac.administrativo.entidades;
+
+public class Perfil {
+
+    private int id;
+    private String nome;
+    private boolean cadastrarFuncionario;
+    private boolean lerFuncionario;
+    private boolean atualizarFuncionario;
+    private boolean deletarFuncionario;
+    private boolean listarFuncionario;
+    private boolean cadastrarPaciente;
+    private boolean lerPaciente;
+    private boolean atualizarPaciente;
+    private boolean deletarPaciente;
+    private boolean listarPaciente;
+    private boolean cadastrarFormulario;
+    private boolean lerFormulario;
+    private boolean atualizarFormulario;
+    private boolean deletarFormulario;
+    private boolean listarFormulario;
+    private boolean cadastrarFspecialidade;
+    private boolean lerEspecialidade;
+    private boolean atualizarEspecialidade;
+    private boolean deletarEspecialidade;
+    private boolean listarEspecialidade;
+    private boolean cadastrarConvenio;
+    private boolean lerConvenio;
+    private boolean atualizarConvenio;
+    private boolean deletarConvenio;
+    private boolean listarConvenio;
+
+}
