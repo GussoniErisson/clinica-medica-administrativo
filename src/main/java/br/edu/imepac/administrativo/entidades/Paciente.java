88c3683ecc534d2c3fefe185ea6b9ad63f59b637
arexandre <alexandrehora2004@gmail.com>
2024-11-13 20:30:17 -0300

    add anotações lombok

--- src/main/java/br/edu/imepac/administrativo/entidades/Paciente.java
+++ src/main/java/br/edu/imepac/administrativo/entidades/Paciente.java
@@ -2,10 +2,19 @@ package br.edu.imepac.administrativo.entidades;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Paciente {
 
     private String nome, sexo, rua, numero, complemento, bairro, cidade, estado, contato, email;
     private Float id, idade, cpf;
     private Date data_nascimento;
+
+
 
 }
