6ba8cf9ddd7b1ab71cab629e1b681c0637a5146b
arexandre <alexandrehora2004@gmail.com>
2024-11-13 19:48:41 -0300

    add class Paciente

---
+++ src/main/java/br/edu/imepac/administrativo/entidades/Paciente.java
@@ -0,0 +1,11 @@
+package br.edu.imepac.administrativo.entidades;
+
+import java.util.Date;
+
+public class Paciente {
+
+    private String nome, sexo, rua, numero, complemento, bairro, cidade, estado, contato, email;
+    private Float id, idade, cpf;
+    private Date data_nascimento;
+
+}
