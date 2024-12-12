7fec338edf75f9e86fe7f2a657de27b630e482c5
Richard Mrqs <richardmarques1002@gmail.com>
2024-12-11 21:58:44 -0300

    DTO, JPA, Repository

--- src/main/java/br/edu/imepac/administrativo/entidades/Prontuario.java
+++ src/main/java/br/edu/imepac/administrativo/entidades/Prontuario.java
@@ -1,9 +1,27 @@
 package br.edu.imepac.administrativo.entidades;
 
-public class Prontuario {
-    private int id;
-    private String receituario;
-    private String exames;
-    private String observacoes;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "prontuario")
+public class Prontuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "receituario", nullable = false)
+    private String receituario;
+
+    @Column(name = "exames")
+    private String exames;
+
+    @Column(name = "observacoes")
+    private String observacoes;
 }
