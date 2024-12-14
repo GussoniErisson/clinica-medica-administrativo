1ad687f89b96c9111e180565848d0be9e42aae04
onidlareg <ggnetom@gmail.com>
2024-12-13 23:50:34 -0300

    Criação da classe PerfilException

---
+++ src/main/java/br/edu/imepac/administrativo/exceptions/PerfilException.java
@@ -0,0 +1,7 @@
+package br.edu.imepac.administrativo.exceptions;
+
+public class PerfilException extends RuntimeException {
+    public PerfilException(String message) {
+        super(message);
+    }
+}
