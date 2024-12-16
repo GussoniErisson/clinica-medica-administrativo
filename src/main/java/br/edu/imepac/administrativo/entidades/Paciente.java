5c1629b83edc035a71a489d23b8d0663d4001edf
arexandre <140089118+arexandre@users.noreply.github.com>
2024-12-16 13:43:44 -0300

    Update Paciente

--- src/main/java/br/edu/imepac/administrativo/entidades/Paciente.java
+++ src/main/java/br/edu/imepac/administrativo/entidades/Paciente.java
@@ -2,6 +2,10 @@ package br.edu.imepac.administrativo.entidades;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,4 +18,21 @@ public class Paciente {
     private String nome, sexo, rua, numero, complemento, bairro, cidade, estado, contato, email;
     private Float id, idade, cpf;
     private Date data_nascimento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "Paciente")
+    private Paciente Paciente;
+
+    @ManyToOne
+    private Paciente paciente;
+
+    @ManyToOne
+    private consulta consulta;
+
+    @ManyToOne
+    private Convenio convenio;
+
+    @ManyToOne
+    private Prontuario prontuario;
+
 }
